1e7462c190e70a0a5bfd6862e797abf644f66aea
Estudiantes <Estudiantes@labing50408.udistrital.edu.co>
2017-08-11 16:51:20 -0500

    Jugando

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -6,10 +6,10 @@ public class Prueba {
         int a = 0;
         int b = 0;
         for (int i = 0; i < 10; i++) {
-            System.out.println("# for");  
-            System.out.println(b);
-            b=b+1;
-        while (a < 100) {
+            System.out.println(b + "# for");
+            b = b + 1;
+            a=0;
+            while (a < 100) {
                 System.out.println("Hola mundo");
                 a = a + 1;
             }
